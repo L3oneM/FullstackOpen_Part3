df1e1e0baaf3b713c4ff3abe9a63a5309b063952
L3oneMclear <apoWebDevAcc@gmail.com>
2019-10-09 19:32:31 +0300

    commit before deployment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+morgan.token('person', function(req, res) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
   {
@@ -46,7 +58,53 @@ app.get('/api/persons/:id', (req, res) => {
   
 })
 
-const PORT = 3001
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+
+  return Math.round(Math.random() * 1000000)
+
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  } 
+
+  let exist = false
+  for(let i = 0; i < persons.length; i++) {
+    if(persons[i].name.toLowerCase() === body.name.toLowerCase()) {
+      exist = true
+    }
+  }
+  
+  if(exist) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+}) 
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
